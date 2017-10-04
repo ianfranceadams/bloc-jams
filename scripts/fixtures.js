6126ee3b5be87a122a34c37746b4e251f0a70159
Ian Adams <ianfranceadams@gmail.com>
2017-10-04 07:17:31 -0500

    Checkpoint 31 jQ

---
+++ scripts/fixtures.js
@@ -0,0 +1,48 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumZeppelin = {
+     title: 'Houses of the Holy',
+     artist: 'Led Zeppelin',
+     label: 'Atlantic',
+     year: '1973',
+     albumArtUrl: 'assets/images/album_covers/22.png',
+     songs: [
+         { title: 'The Song Remains the Same', duration: '5:32' },
+         { title: 'The Rain Song', duration: '7:39' },
+         { title: 'Over the Hills and Far Away', duration: '4:50'},
+         { title: 'The Crunge', duration: '3:17' },
+         { title: 'Dancing Days', duration: '3:43'},
+         { title: 'D\'yer Mak\'er', duration: '4:23'},
+         { title: 'No Quarter', duration: '7:00'},
+         { title: 'The Ocean', duration: '4:31'}
+     ]
+ };
